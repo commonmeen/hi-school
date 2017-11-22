971b1674bf6b03aa089adb0c95ed34fae21e2f94
NonRn <rn.non@hotmail.com>
2017-11-22 17:54:18 +0700

    splash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2' ;
+import { AngularFireDatabaseModule } from 'angularfire2/database' ;
+
+export const config = {
+  apiKey: "AIzaSyA0pHeRyxU_Fx-Ep6_5SgDuZp-Gz84CnMA",
+  authDomain: "hi-school-8c626.firebaseapp.com",
+  databaseURL: "https://hi-school-8c626.firebaseio.com",
+  projectId: "hi-school-8c626",
+  storageBucket: "",
+  messagingSenderId: "468596081059"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +25,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
